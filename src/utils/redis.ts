61ecdbafd20634592e003010375f3fed655dd0f2
jweboy <1259524459@qq.com>
2022-05-30 15:30:18 +0800

    feat(redis): 更新配置项，新增密码校验和指定数据库

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -7,13 +7,15 @@ import { createClient } from 'redis';
  * @LastEditTime: 2021-12-31 18:21:59
  */
 
-const { ALIYUN_REDIS, NODE_ENV, REDIS_CONTAINER_NAME } = process.env;
+const { ALIYUN_REDIS, NODE_ENV, REDIS_CONTAINER_NAME, REDIS_PASSWORD } = process.env;
 
 export const initRedis = async () => {
   const isDev = NODE_ENV === 'development';
   const isProd = NODE_ENV === 'production';
   // const client = new Redis(6379, "redis");
   const client = createClient({
+    password: REDIS_PASSWORD,
+    database: 1,
     ...(isDev && { url: ALIYUN_REDIS }),
     ...(isProd && {
       socket: { host: REDIS_CONTAINER_NAME },
@@ -21,7 +23,10 @@ export const initRedis = async () => {
   });
 
   client
-    .on('error', (err) => console.log('Redis Client Error', err))
+    .on('error', (err) => {
+      console.log('Redis Client Error', err);
+      client.disconnect();
+    })
     .on('ready', () => {
       console.log('🤫 Redis client is ready.');
     });
